ed3f3670cf24e435b4c9bc54c7819150cbe2dc9c
enki <wzrdclvb@gmail.com>
2026-01-06 16:03:01 -0500

    chore: add open-next.config.ts

---
+++ open-next.config.ts
@@ -0,0 +1,10 @@
+import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+
+export default defineCloudflareConfig({
+    /*
+     * The OpenNext configuration.
+     *
+     * You can override the default configuration here.
+     * For more information, see https://opennext.js.org/cloudflare/config
+     */
+});
